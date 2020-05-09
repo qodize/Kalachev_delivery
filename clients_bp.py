7e5f435d98f4c36304feab4ac4532c7d50d6d40d
nightsea987 <shafigullina20031015@gmail.com>
2020-05-09 23:26:02 +0500

    added address and client forms

--- clients_bp.py
+++ clients_bp.py
@@ -1,4 +1,8 @@
 from flask import Blueprint, render_template
+from client_form import ClientForm
+from address_form import AddressForm
+from data.users import User
+from data import db_session
 
 blueprint = Blueprint('clients_bp', __name__,
                       template_folder='templates')
@@ -16,4 +20,17 @@ def client_home():
 
 @blueprint.route('/profile')
 def profile():
-    return render_template('profile.html', title='Профиль')
+    form = ClientForm()
+    form_address = AddressForm()
+    # if form.validate_on_submit():
+    #     session = db_session.create_session()
+    #     if session.query(User).filter(User.email == form.email.data).first():
+    #         return render_template(
+    #             "profile.html",
+    #             title='Профиль',
+    #             form_modal_edit=form,
+    #             message='Пользователь с таким email уже существует.'
+    #         )
+    # я не панимаю, но здесь должно быть что-то вроде этого
+
+    return render_template('profile.html', title='Профиль', form_modal_edit=form, form_address=form_address)
