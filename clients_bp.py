95f1a48150bb96e974da0bb7e223944c24f19f13
= <qodize@gmail.com>
2020-05-10 04:17:26 +0500

    bagfix, added functional client login/register, client profile

--- clients_bp.py
+++ clients_bp.py
@@ -1,13 +1,13 @@
 from flask import Blueprint, render_template, redirect
-from client_form import ClientForm
-from address_form import AddressForm
+from forms.client_form import ClientForm
+from forms.address_form import AddressForm
 from login_required import login_required
 from flask_login import current_user
 from main import login_user
 from forms.client_login_form import ClientLoginForm
 
 from data import db_session
-from data.users import User
+from data.users import User, ClientAddress
 from data.orders import Order
 
 blueprint = Blueprint('clients_bp', __name__,
@@ -15,7 +15,7 @@ blueprint = Blueprint('clients_bp', __name__,
 
 
 #  Авторизация
-@blueprint.route('/login', methods=['GET', 'POST'])
+@blueprint.route('/login/', methods=['GET', 'POST'])
 def client_login():
     form = ClientLoginForm()
     if form.validate_on_submit():
@@ -52,19 +52,27 @@ def index():
 def profile():
     form = ClientForm()
     form_address = AddressForm()
-    # if form.validate_on_submit():
-    #     session = db_session.create_session()
-    #     if session.query(User).filter(User.email == form.email.data).first():
-    #         return render_template(
-    #             "profile.html",
-    #             title='Профиль',
-    #             form_modal_edit=form,
-    #             message='Пользователь с таким email уже существует.'
-    #         )
-    # я не панимаю, но здесь должно быть что-то вроде этого
-
+    session = db_session.create_session()
+    user = session.query(User).filter(User.id == current_user.id).first()
+    if form.validate_on_submit():
+        user.name = form.name.data
+        user.surname = form.surname.data
+        user.email = form.email.data
+        user.birthday = '.'.join([form.day.data, form.month.data, form.year.data])
+        session.merge(user)
+        session.commit()
+    if form_address.validate_on_submit():
+        address = ClientAddress(
+            client=user,
+            data=f'г.{form_address.city.data}, ул. {form_address.street.data}, д. {form_address.house_number.data}, ' +
+            f'кв. {form_address.flat.data}, подъезд {form_address.entrance.data}, этаж {form_address.floor.data}, ' +
+            f'код домофона {form_address.doorphone.data}'
+        )
+        session.add(address)
+        session.commit()
+    user.client_orders = list(reversed(user.client_orders))
     return render_template('profile.html', title='Профиль', form_modal_edit=form,
-                           form_address=form_address, current_user=current_user)
+                           form_address=form_address, current_user=user)
 
 
 #  Корзина
--- forms/client_login_form.py
+++ forms/client_login_form.py
@@ -1,5 +1,8 @@
 from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 
 class ClientLoginForm(FlaskForm):
-    pass
+    phone_number = StringField("Номер телефона", validators=[DataRequired()])
+    submit = SubmitField("ПОЛУЧИТЬ КОД")
