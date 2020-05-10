487c40ab73f1f85c5501d0d7770bb8f6be094d83
= <qodize@gmail.com>
2020-05-11 00:54:39 +0500

    added categories

---
+++ data/categories.py
@@ -0,0 +1,15 @@
+import sqlalchemy as sa
+from sqlalchemy import orm
+from .db_session import SqlAlchemyBase
+
+
+class Category(SqlAlchemyBase):
+    __tablename__ = 'categories'
+
+    id = sa.Column(sa.Integer, primary_key=True, autoincrement=True)
+    name = sa.Column(sa.String, nullable=True)
+    describtion = sa.Column(sa.String, nullable=True)
+
+    products = orm.relation('Product',
+                            back_populates='category',
+                            foreign_keys='Product.category_id')
